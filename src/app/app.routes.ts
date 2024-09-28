4b49024c95293cb3355ad4de7f44eca86a502895
aparna-sys <aparnaKK1999@gmail.com>
2024-09-28 11:36:15 +0530

    DigniLift Frontend First Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ResourcesComponent } from './components/resources/resources.component';
+import { SuccessStoriesComponent } from './components/success-stories/success-stories.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'login',component:LoginComponent},
+    {path:'profile',component:ProfileComponent},
+    {path:'resources',component:ResourcesComponent},
+    {path:'success-stories',component:SuccessStoriesComponent},
+    {path:'',redirectTo:'/login',pathMatch:'full'},
+];
